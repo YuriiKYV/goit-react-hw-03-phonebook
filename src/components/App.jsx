2bcf0e72e617ba6bc31f8ba3b5ed29c0577a2d74
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-10-12 23:04:38 +0300

    Completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,63 @@
 import React, { Component } from 'react';
-import ContactForm from "./ContactForm/ContactForm";
+import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
 import { nanoid } from 'nanoid';
 
-
 export default class App extends Component {
   state = {
     contacts: [],
-  filter: ''
-  }
-  
-  addContact = (contact) => {
-    if (this.isDublicate(contact)) {
-      return alert(`${contact.name} is already in contacts.`)
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts && contacts.length) {
+      this.setState({
+        contacts,
+      });
     }
-    this.setState((prev) => {
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  addContact = contact => {
+    if (this.isDublicate(contact)) {
+      return alert(`${contact.name} is already in contacts.`);
+    }
+    this.setState(prev => {
       const newContact = {
         id: nanoid(),
-        ...contact
-      }
-        return {
-    contacts: [...prev.contacts, newContact]
-      }
-    })
-  }
+        ...contact,
+      };
+      return {
+        contacts: [...prev.contacts, newContact],
+      };
+    });
+  };
 
-  removeContact = (id) => {
-    this.setState((prev) => {
-      const newContact = prev.contacts.filter((item) => item.id !== id);
+  removeContact = id => {
+    this.setState(prev => {
+      const newContact = prev.contacts.filter(item => item.id !== id);
 
       return {
-        contacts: newContact
-      }
-    })
-  }
+        contacts: newContact,
+      };
+    });
+  };
 
-  handleChange = (e) => {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value
-        })
-    }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
   getFilteredContacts() {
     const { contacts, filter } = this.state;
@@ -54,30 +69,32 @@ export default class App extends Component {
     const filteredContacts = contacts.filter(({ name }) => {
       const normalizedName = name.toLocaleLowerCase();
       const resault = normalizedName.includes(normalizedFilter);
-      return resault
-    })
+      return resault;
+    });
     return filteredContacts;
   }
 
   isDublicate({ name }) {
     const { contacts } = this.state;
-    const resault = contacts.find((item) => item.name.toLocaleLowerCase() === name.toLocaleLowerCase());
+    const resault = contacts.find(
+      item => item.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+    );
     return resault;
   }
-  
+
   render() {
     const books = this.getFilteredContacts();
     const { addContact, removeContact, handleChange } = this;
-  
-      return (
-        <div className="App">
-          <h1>Phonebook</h1>
-          <ContactForm onSubmit={addContact}></ContactForm>
-          
-          <h2>Contacts</h2>
-          <Filter filter={this.state.filter} handleChange={handleChange} />
-          <ContactList arrayContacts={books} removeContact={removeContact} />
-        </div>
-      )
-    }
+
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={addContact}></ContactForm>
+
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} handleChange={handleChange} />
+        <ContactList arrayContacts={books} removeContact={removeContact} />
+      </div>
+    );
+  }
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,25 @@
 import { PropTypes } from 'prop-types';
 
 export default function ContactList({ arrayContacts, removeContact }) {
-    const items = arrayContacts.map(({ name, number, id}) => {
-        return <li key={id}>{name}: {number} <button onClick={() => removeContact(id)}>Delete</button></li>
-    })
+  console.log(arrayContacts);
+  const items = arrayContacts.map(({ name, number, id }) => {
     return (
-        <ul>{items}</ul>
-    )
+      <li key={id}>
+        {name}: {number}{' '}
+        <button onClick={() => removeContact(id)}>Delete</button>
+      </li>
+    );
+  });
+  return <ul>{items}</ul>;
 }
 
 ContactList.propTypes = {
-    arrayContacts: PropTypes.array.isRequired,
-    removeContact: PropTypes.func.isRequired,
+  arrayContacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  removeContact: PropTypes.func.isRequired,
 };
